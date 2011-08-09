a222eb4192a28015381ea3f71ac57e455062cfc0
Rene Cunningham <rene@rene.bz>
2011-08-09 20:00:26 +1000

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class cron { noop }
+
+# http://linuxman.wikispaces.com/Creating+a+cron+job+with+puppet
+# manage cron jobs in separate files - call with enable => "false" to delete the job
+define cron_job( $enable = "true", $interval = "daily", $script = "", $package = "" ) {
+	file { "/etc/cron.$interval/$name":
+		content         => $script,
+		ensure          => $enable ? {
+			"false" => absent,
+			default => file,
+		},
+		force           => true,
+		owner           => root,
+		group           => root,
+		mode            => $interval ? {
+			"d"     => 644,
+			default => 755,
+		},
+		require         => $package ? {
+			""      => undef,
+			default => Package[$package],
+		},
+	}
+}
